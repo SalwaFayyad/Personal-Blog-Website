dcf3acbd35bf6ce04a6ca8cee54637fc74b80e64
SalwaFayyad <104863637+SalwaFayyad@users.noreply.github.com>
2024-07-28 16:11:00 +0300

    Initial commit

---
+++ script/script.js
@@ -0,0 +1,9 @@
+
+function toggleMenu() {
+    var nav = document.getElementById("nav_header2");
+    if (nav.style.display === "block") {
+        nav.style.display = "none";
+    } else {
+        nav.style.display = "block";
+    }
+}
